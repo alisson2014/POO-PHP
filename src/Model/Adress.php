49754e88f6d634e68896268552f846e88cd6909e
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-04-15 10:54:46 -0300

    apliquei os métodos__get, __set e __toString

--- src/Model/Adress.php
+++ src/Model/Adress.php
@@ -2,6 +2,15 @@
 
 namespace POO_PHP\Bank\Model;
 
+/*
+    * class Adress
+    * @package POO_PHP\Bank\Model
+    * @property-read string $city
+    * @property-read string $neighborhood
+    * @property-read string $road
+    * @property-read string $number
+*/
+
 class Adress
 {
     private string $city;
@@ -21,43 +30,60 @@ class Adress
         $this->number = $number;
     }
 
-    public function getCity(): string
+    private function getCity(): string
     {
         return $this->city;
     }
 
-    public function getNeighborhood()
+    private function getNeighborhood(): string
     {
         return $this->neighborhood;
     }
 
-    public function getRoad(): string
+    private function getRoad(): string
     {
         return $this->road;
     }
 
-    public function getNumber(): string
+    private function getNumber(): string
     {
         return $this->number;
     }
 
-    public function setCity(string $newCity): void
+    private function setCity(string $newCity): void
     {
         $this->city = $newCity;
     }
 
-    public function setNeighborhood(string $newNeighborhood): void
+    private function setNeighborhood(string $newNeighborhood): void
     {
         $this->neighborhood = $newNeighborhood;
     }
 
-    public function setRoad(string $newRoad): void
+    private function setRoad(string $newRoad): void
     {
         $this->road = $newRoad;
     }
 
-    public function setNumber(string $newNumber): void
+    private function setNumber(string $newNumber): void
     {
         $this->number = $newNumber;
+    }
+
+    public function __get(string $method)
+    {
+        $method = "get" . ucfirst($method);
+        return $this->$method();
+    }
+
+    public function __set(string $method, string $value)
+    {
+        $method = "set" . ucfirst($method);
+        return $this->$method($value);
+    }
+
+    public function __toString(): string
+    {
+        return "{$this->road}, {$this->number}, {$this->neighborhood}, {$this->city}";
     }
 }
