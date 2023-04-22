dc0d6a824926c63c012a467f13d86d68f5870cb8
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-04-22 18:05:18 -0300

    feat: adicionei annotations

--- tests/adresses.php
+++ tests/adresses.php
@@ -7,12 +7,7 @@ use POO_PHP\Bank\Model\Adress;
 $adress = new Adress("São paulo", "Butânta", "Rua joão garcia", "122");
 
 //Mostra todos os campos
-echo $adress . PHP_EOL;
-//Mostra um a um
-echo $adress->city . PHP_EOL;
-echo $adress->neighborhood . PHP_EOL;
-//Chama o setter do campo
-$adress->city = "Rio de janeiro";
-$adress->neighborhood = "Tijuca";
-//Mostra todos os campos alterados
-echo $adress . PHP_EOL;
+echo "Rua: {$adress->road}" . PHP_EOL;
+echo "Número: {$adress->number}" . PHP_EOL;
+echo "Bairro: {$adress->neighborhood}" . PHP_EOL;
+echo "Cidade: {$adress->city}" . PHP_EOL;
