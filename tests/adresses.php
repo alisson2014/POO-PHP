471cd03ac4967bf272f1898f6824be61b2ffc7d9
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-04-22 18:28:33 -0300

    testei a classe adress

--- tests/adresses.php
+++ tests/adresses.php
@@ -6,8 +6,13 @@ use POO_PHP\Bank\Model\Adress;
 
 $adress = new Adress("São paulo", "Butânta", "Rua joão garcia", "122");
 
-//Mostra todos os campos
-echo "Rua: {$adress->road}" . PHP_EOL;
-echo "Número: {$adress->number}" . PHP_EOL;
-echo "Bairro: {$adress->neighborhood}" . PHP_EOL;
-echo "Cidade: {$adress->city}" . PHP_EOL;
+//Mostra todos os campos (método toString())
+echo $adress;
+
+//Altera o bairro (método __set())
+$adress->neighborhood = "Itaquera";
+
+//Mostra o bairro (método __get())
+echo $adress->neighborhood . PHP_EOL;
+
+echo $adress;
