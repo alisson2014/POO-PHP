b42a1c9304ee2434bbb5be63a035b9e211e3a0ee
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-04-22 19:03:13 -0300

    feat: adicionei tratamento para erros

--- tests/accounts.php
+++ tests/accounts.php
@@ -2,9 +2,18 @@
 
 require_once "autoload.php";
 
-use POO_PHP\Bank\Model\Account\{CheckingAccount, Client};
+use POO_PHP\Bank\Model\Account\{CheckingAccount, Client, InsufficientAmountException};
 use POO_PHP\Bank\Model\Adress;
 
 $adress = new Adress("Campo grande", "Palmares", "Rua C. Índio Bandeira", "15a");
 $client = new Client("João inácio", "115.466.811-12", $adress);
 $account = new CheckingAccount($client);
+
+echo $account->deposit(500) . PHP_EOL;
+
+//Tratando erros
+try {
+    echo $account->withdraw(600) . PHP_EOL;
+} catch (InsufficientAmountException $e) {
+    echo "Saldo insulficiente" . PHP_EOL;
+}
