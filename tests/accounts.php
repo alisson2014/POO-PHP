c5c8b55557252e071940fe8f53c1ed7a86e17d96
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-04-23 15:20:14 -0300

    testando as contas

--- tests/accounts.php
+++ tests/accounts.php
@@ -3,19 +3,53 @@
 require_once "autoload.php";
 
 use POO_PHP\Bank\Model\Account\{CheckingAccount, Client, InsufficientAmountException};
+use POO_PHP\Bank\Model\Account\SavingsAccount;
 use POO_PHP\Bank\Model\Adress;
 
-$adress = new Adress("Campo grande", "Palmares", "Rua C. Índio Bandeira", "15a");
-$client = new Client("João", "115.465.445-46", $adress);
-$account = new CheckingAccount($client);
+//Endereços
+$adress1 = new Adress("Campo grande", "Palmares", "Rua C. Índio Bandeira", "15a");
+$adress2 = new Adress("São Paulo", "Tatuapé", "Rua são vicente", "160");
 
-echo $account->deposit(500) . PHP_EOL;
+//Clientes
+$client1 = new Client("João", "115.465.445-46", $adress1);
+$client2 = new Client("Eduarda", "113.466.188-46", $adress2);
 
-//Tratando erros
+//Contas
+$account1 = new CheckingAccount($client1);
+$account2 = new SavingsAccount($client2);
+
+//Mostra endereço
+echo $adress1 . PHP_EOL;
+
+//Muda um valor do endereço
+$adress1->city = "Cuiaba";
+echo $adress1->city;
+
+//Mostra os dados(var_dump) do cliente
+echo var_dump($client1);
+
+//Método de deposito
+$account1->deposit(1500);
+echo $account1->balance . PHP_EOL;
+
+$account2->deposit(1800);
+echo $account2->balance . PHP_EOL;
+
+//Método de saque
+$account1->withdraw(100);
+echo $account1->balance . PHP_EOL;
+
+$account2->withdraw(600);
+echo $account2->balance . PHP_EOL;
+
+//Método de transferência
+echo $account1->transfer(1100, $account2) . PHP_EOL;
+
+//Tratando erros (Saldo insulficiente)
 try {
-    echo $account->withdraw(600) . PHP_EOL;
+    echo $account1->withdraw(600) . PHP_EOL;
 } catch (InsufficientAmountException $e) {
     echo "Saldo insulficiente" . PHP_EOL;
+} finally {
+    echo "Operação finalizada.";
 }
-
-echo $client->name;
